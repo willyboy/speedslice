9f61f47abf41d2653a0e065b63d8afd526632e9f
William Neely <william@piecewise.com>
2013-04-26 12:51:01 -0400

    Update for sign up pizzas

--- js/speedslice.js
+++ js/speedslice.js
@@ -41,6 +41,17 @@ function checkConnection(){
         states[Connection.NONE]     = 'No network connection';
 
         alert('Connection type: ' + states[networkState]);
+		var networkState = navigator.network.connection.type;
+	var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        alert('Connection type: ' + states[networkState]);
 	if(networkState==Connection.NONE){
 		navigator.notification.alert("SpeedSlice requires an active internet connection.",checkConnection,"SpeedSlice","Okay");
 	}
