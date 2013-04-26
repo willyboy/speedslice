b26fa0c61a13ee116eaf7999f46036d4ee7b022e
William Neely <william@piecewise.com>
2013-04-26 12:55:15 -0400

    Update for sign up pizzas

--- js/speedslice.js
+++ js/speedslice.js
@@ -30,28 +30,7 @@ function onDeviceReady() {
 	pushNotification = window.plugins.pushNotification;
 }
 function checkConnection(){  
-	var networkState = navigator.connection.type;
-	var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.NONE]     = 'No network connection';
-
-        alert('Connection type: ' + states[networkState]);
-		var networkState = navigator.network.connection.type;
-	var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.NONE]     = 'No network connection';
-
-        alert('Connection type: ' + states[networkState]);
+	var networkState = navigator.network.connection.type;//needs to be navigator.connection if phonegap updated.
 	if(networkState==Connection.NONE){
 		navigator.notification.alert("SpeedSlice requires an active internet connection.",checkConnection,"SpeedSlice","Okay");
 	}
